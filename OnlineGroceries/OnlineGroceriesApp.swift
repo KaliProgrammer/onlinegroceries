ae1c7dfa906c75dfc90b9c98507354934d3ecf16
Alyona <alena.korchak@yournextagency.com>
2025-03-03 12:14:13 +0300

    Initial commit

---
+++ OnlineGroceries/OnlineGroceriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnlineGroceriesApp.swift
+//  OnlineGroceries
+//
+//  Created by MacBook Air on 03.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct OnlineGroceriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ OnlineGroceriesTests/OnlineGroceriesTests.swift
@@ -0,0 +1,17 @@
+//
+//  OnlineGroceriesTests.swift
+//  OnlineGroceriesTests
+//
+//  Created by MacBook Air on 03.03.2025.
+//
+
+import Testing
+@testable import OnlineGroceries
+
+struct OnlineGroceriesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
